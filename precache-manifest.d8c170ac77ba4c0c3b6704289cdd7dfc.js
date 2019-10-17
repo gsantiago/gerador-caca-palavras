308659b514098e0915118728ccbb1c953b6aa11f
Guilherme Santiago <guilherme.santiago95@gmail.com>
2019-10-17 11:11:42 -0300

    Updates

---
+++ precache-manifest.d8c170ac77ba4c0c3b6704289cdd7dfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9b2768e70397eade36592299e5b846",
+    "url": "/gerador-de-caca-palavras/index.html"
+  },
+  {
+    "revision": "763d235a0d0e70331f34",
+    "url": "/gerador-de-caca-palavras/static/css/main.cf11fda9.chunk.css"
+  },
+  {
+    "revision": "55a4e3c240e2bda318ff",
+    "url": "/gerador-de-caca-palavras/static/js/2.fbdda6d6.chunk.js"
+  },
+  {
+    "revision": "763d235a0d0e70331f34",
+    "url": "/gerador-de-caca-palavras/static/js/main.41a86df5.chunk.js"
+  },
+  {
+    "revision": "16fd8168b9abeb5cb29b",
+    "url": "/gerador-de-caca-palavras/static/js/runtime-main.962cbbab.js"
+  }
+]);
